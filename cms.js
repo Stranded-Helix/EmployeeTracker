8dd24ff60c7530ab4644219f821c30b3dbc70b79
Garrett Fite <garrett.fite@gmail.com>
2021-05-01 23:52:11 -0500

    added functions for menu interactions

--- cms.js
+++ cms.js
@@ -1,6 +1,7 @@
 const mysql = require(`mysql`);
 const inquirer = require(`inquirer`);
-const database = require(`./database.js`)
+const database = require(`./database.js`);
+const { fetchAsyncQuestionPropertyQuestionProperty } = require("inquirer/lib/utils/utils");
 
 const start = () => {
     database.connectDatabase()
@@ -13,26 +14,45 @@ const mainMenu = () => {
         type: `list`,
         message: `What would you like to do?`,
         choices: [
+            `View All Employees`,
             `View All Employees By Department`,
             `View All Employees By Manager`,
+            `View Departments`,
+            `View Roles`,
             `Add Employee`,
+            `Add Department`,
+            `Add Role`,
             `Remove Employee`,
             `Update Employee Role`,
             `Update Employee Manager`,
-            `View All Roles`
         ],
         name: `mainMenuChoice`
     })
     .then(response => {
         switch(response.mainMenuChoice) {
+            case `View All Employees`:
+                employeesAll();
+                break;
             case `View All Employees By Department`:
                 employeesByDepartment();
                 break;
             case `View All Employees By Manager`:
                 employeesByManager();
                 break;
+            case `View Departments`:
+                departmentsAll();
+                break;
+            case `View Roles`:
+                rolesAll();
+                break;
             case `Add Employee`:
                 employeesAdd();
+                break;
+            case `Add Department`:
+                departmentAdd();
+                break;
+            case `Add Role`:
+                roleAdd();
                 break;
             case `Remove Employee`:
                 employeesRemove();
@@ -43,15 +63,26 @@ const mainMenu = () => {
             case `Update Employee Manager`:
                 employeesUpdateManager();
                 break;
-            case `View All Roles`:
-                rolesView();
-                break;
-
         }
     })
     
 }
 
+const employeesAll = () => {
+    database.selectEmployee()
+    .then(selectEmployeeResults => console.table(selectEmployeeResults.map(x => x.name)))
+    .then(() => mainMenu())
+}
+const departmentsAll = () => {
+    database.selectDepartments()
+    .then(selectDepartmentResults => console.table(selectDepartmentResults))
+    .then(() => mainMenu())
+}
+const rolesAll = () => {
+    database.selectRoles()
+    .then(selectRolesResults => console.table(selectRolesResults.map(x => x.name)))
+    .then(() => mainMenu())
+}
 const employeesByDepartment = () => {
     database.selectDepartments()
     .then(selectDepartmentsResults => {
@@ -64,22 +95,82 @@ const employeesByDepartment = () => {
             }
         )
         .then(departmentChoiceResults => {
-            database.selectEmployeesByDepartment(departmentChoiceResults.departementChoice);
+            database.selectEmployeesByDepartment(departmentChoiceResults.departementChoice)
+            .then(()=> mainMenu());
+        })
+    })
+}
+const employeesByManager = () => {
+    database.selectManagers()
+    .then(selectManagersResults => {
+        inquirer.prompt(
+            {
+                type: `list`,
+                message: `Whose team would you like to view`,
+                choices:  selectManagersResults,
+                name: `managerChoice`
+            }
+        )
+        .then(managerChoiceResults => {
+            database.selectEmployeesByManager(managerChoiceResults.managerChoice)
+            .then(()=> mainMenu());
         })
     })
 }
 
-
-const employeesByManager = () => {
-    
-}
-
 const employeesAdd = () => {
-    
+    Promise.all([database.selectRoles(), database.selectManagers()])
+    .then((promiseResults) => {
+        promiseResults[1].push({name: `No Manager`, value: -1})
+        console.log(promiseResults);
+    inquirer.prompt([
+        {
+            type: `input`,
+            message: `First Name`,
+            name: `firstName`
+        },
+        {
+            type: `input`,
+            message: `Last Name`,
+            name: `lastName`
+        },
+        {
+            type: `list`,
+            message: `Assign Role`,
+            choices: promiseResults[0],
+            name: `role`
+        },
+        {
+            type: `list`,
+            message: `Assign Manager`,
+            choices: promiseResults[1],
+            name: `manager`
+        },
+        ]
+        )
+        .then((inqResults) => {
+            if(inqResults.manager === -1) delete inqResults.manager
+            database.insertEmployee(inqResults)
+        })
+        .then(() => mainMenu())
+    })
 }
 
 const employeesRemove = () => {
-    
+    database.selectEmployee()
+    .then(selectEmployeeResults => {
+        console.log(selectEmployeeResults);
+        inquirer.prompt({
+            type: `list`,
+            message: `Which employee are you removing`,
+            choices: selectEmployeeResults,
+            name: `employeeChoice`,
+        })
+        .then(employeeChoiceResults => {
+            database.deleteEmployee(employeeChoiceResults.employeeChoice)
+        })
+        .then(() => mainMenu())
+    })
 }
 
 const employeesUpdateRole = () => {
@@ -90,9 +181,6 @@ const employeesUpdateManager = () => {
     
 }
 
-const rolesView = () => {
-    
-}
 
 //Function Calls
 start();
--- database.js
+++ database.js
@@ -1,57 +1,160 @@
 const mysql = require(`mysql`);
 
-var connection; 
+var connection;
 
 const connectDatabase = () => {
-return new Promise ((resolve, reject) => {
-    connection = mysql.createConnection({
-    host: `localhost`,
-    user: `root`,
-    password: `SMUPassword`,
-    database: `employee_tracker_db`
-});
-connection.connect((err) => {
-    if (err) reject(`Failed to Connect to Database`);
-    resolve(connection);
-})
-})};
-
-const selectDepartments = () => { 
-    return new Promise ((resolve, reject) => {
-        connection.query(`SELECT department_name FROM department`, (err, res) => {
-            if (err) reject(`Failed selectDepartment`);
-            resolve(res.map(x => x.department_name));
+    return new Promise((resolve, reject) => {
+        connection = mysql.createConnection({
+            host: `localhost`,
+            user: `root`,
+            password: `SMUPassword`,
+            database: `employee_tracker_db`
         });
-});
+        connection.connect((err) => {
+            if (err) reject(`Failed to Connect to Database`);
+            resolve(connection);
+        })
+    })
+};
+
+const selectDepartments = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT department_name 
+        FROM department`,
+            (err, res) => {
+                if (err) reject(`Failed selectDepartment`);
+                resolve(res.map(x => x.department_name));
+            });
+    });
 }
 
-// const selectDepartments = new Promise ((resolve, reject) => {
-//     connection.connect((err) => {
-//         connection.query(`SELECT department_name FROM department`, (err, res) => {
-//             if (err) reject(`Failed selectDepartment`);
-//             resolve(res.map(x => x.department_name));
-//         });
-//     })
-// });
-
 const selectEmployeesByDepartment = (department) => {
-    return new Promise((resolve, reject) => { 
-        connection.query(`SELECT employees.first_name, employees.last_name, employee_role.title FROM employees JOIN employee_role ON employees.role_id = employee_role.id JOIN department ON employee_role.department_id = department.id WHERE department_name="${department}"`,
-            // [
-            //     {
-            //         department_name: department
-            //     }
-            // ],
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT employees.first_name, employees.last_name, employee_role.title
+        FROM employees
+        JOIN employee_role
+        ON employees.role_id = employee_role.id
+        JOIN department ON employee_role.department_id = department.id
+        WHERE ?`,
+            [
+                {
+                    department_name: department
+                }
+            ],
             (err, res) => {
-                if (err) reject(console.log(`Failed sEBD:  ${queryString.sql}`));
-                resolve(console.log(res))
+                if (err) reject(console.log(`Failed selectEmployeesByDepartment`));
+                resolve(console.table(res))
             })
-            if(err) throw err;
     })
 }
 
+const selectManagers = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT managers.id, managers.first_name, managers.last_name, employee_role.title
+        FROM employees managers
+        JOIN employee_role
+        ON managers.role_id = employee_role.id
+        JOIN employees
+        ON managers.id = employees.manager_id
+        GROUP BY managers.id`,
+            (err, res) => {
+                if (err) reject(console.log(`Failed selectManagers`));
+                resolve(res.map(x => ({name: `${x.first_name} ${x.last_name}: ${x.title}`, value: x.id}))
+                )
+            })
+    })
+}
+
+const selectEmployeesByManager = (manager) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT employees.first_name, employees.last_name, employee_role.title
+        FROM employees
+        JOIN employee_role
+        ON employees.role_id = employee_role.id
+        WHERE ?`,
+            [
+                {
+                    manager_id: manager
+                }
+            ],
+            (err, res) => {
+                if (err) reject(console.log(`Failed selectEmployeesByManager`));
+                resolve(console.table(res))
+            });
+    })
+}
+
+const selectRoles = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT id, title 
+        FROM employee_role`,
+            (err, res) => {
+                if (err) reject(`Failed selectRoles`);
+                resolve(res.map(x => ({name: x.title, value: x.id})));
+            });
+    });
+}
+
+const selectEmployee = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT id, first_name, last_name 
+        FROM employees`,
+            (err, res) => {
+                if (err) reject(`Failed selectEmployee`);
+                resolve(res.map(x => ({name: `${x.first_name} ${x.last_name}`, value: x.id})));
+            });
+    });
+}
+
+const insertEmployee = (employee) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `INSERT INTO employees SET ?`,
+            [
+                {
+                    first_name: employee.firstName,
+                    last_name: employee.lastName,
+                    role_id: employee.role,
+                    manager_id: employee.manager
+                }
+            ],
+            (err, res) => {
+                if (err) reject(console.log(`Failed insertEmployee`));
+                resolve(res)
+            });
+    })
+}
+
+const deleteEmployee = (employeeId) => {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `DELETE FROM employees WHERE ?`,
+            [
+                {
+                    id: employeeId
+                }
+            ],
+            (err, res) => {
+                if (err) reject(console.log(`Failed deleteEmployee`));
+                resolve(res)
+            });
+    })
+}
 module.exports = {
     connectDatabase,
     selectDepartments,
-    selectEmployeesByDepartment
+    selectEmployeesByDepartment,
+    selectManagers,
+    selectEmployeesByManager,
+    selectRoles,
+    insertEmployee,
+    selectEmployee,
+    deleteEmployee
+
 }
